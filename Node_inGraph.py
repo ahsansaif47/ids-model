f84b5368e75308abde6b1db3d1fd74a266495d6a
ahsansaif47 <ahsan_saif47@hotmail.com>
2021-12-17 22:10:34 +0500

    Check node in a graph

--- Node_inGraph.py
+++ Node_inGraph.py
@@ -1,9 +1,25 @@
 import networkx as nx
+import pandas as pd
+import os
+from Mat_Package import Grapher
+import numpy as np
 
 bG = nx.read_gpickle("./Graph DS/Graph.gpickle")
 
-if 'date' in bG:
-    print("Present in Graph..")
+CSV_Folder = r"./Test Invoices Folder/CSVs/"
+csvs = os.listdir(CSV_Folder)
+print(CSV_Folder + csvs[0])
+df = pd.read_csv(CSV_Folder + csvs[0])
 
-else:
-    print("Not in Graph")
+G = Grapher.makeGraph(df)
+
+Nodes = G.nodes()
+
+print('remark' in Nodes)
+
+N_list = []
+
+for N in Nodes:
+    N_list.append(N)
+
+print(len(N_list))
